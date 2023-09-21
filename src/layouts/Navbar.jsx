488d81146a764d21a19477a232821395d43236de
Mehedi0101 <mehedi29121998@gmail.com>
2023-09-21 08:27:25 +0600

    initial commit

---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Emart</h1>
+            <div>
+                <NavLink></NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import route from './route/route'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider route={route}></RouterProvider>
+  </React.StrictMode>,
+)
